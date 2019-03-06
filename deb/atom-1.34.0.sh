8b47c7ff1b5c48e7f5a53c205d0ceedb85e636ac
Dindin Hernawan <dindin_hernawan@yahoo.com>
2019-03-06 12:48:39 +0700

    Update atom (1.34.0)

--- deb/atom-1.34.0.sh
+++ deb/atom-1.34.0.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-wget -c https://atom-installer.github.com/v1.34.0/atom-amd64.deb
+# wget -c https://atom-installer.github.com/v1.34.0/atom-amd64.deb
+mv atom-amd64.deb atom_1.34.0_amd64.deb
+dpkg-sig -k 959CDDD1 --sign bundler atom_1.34.0_amd64.deb
+mv atom_1.34.0_amd64.deb ../../
